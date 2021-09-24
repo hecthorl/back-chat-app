e79789c7833738549426d87d288d258fd1afc767
hecthorl <hlvm03@gmail.com>
2021-09-24 09:37:04 -0500

    new approaches

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const http = require("http");
+// const jjj = require("./bdRooms.json");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 
 const { Server } = require("socket.io");
+const saveDB = require("./saveDB");
 
 const PORT = process.env.PORT || 4000;
 
@@ -14,18 +17,22 @@ const io = new Server(server, {
       methods: ["GET", "POST"],
    },
 });
-
-app.get("/", (req, res) => {
-   res.status(200).send("Todo OK");
-});
+app.use(cors());
+// app.get("/roominfo", (req, res) => {
+//    const { roomId } = req.query;
+//    const roomData = jjj.filter(item => item.roomId === roomId);
+//    res.status(200).send(roomData);
+// });
 
 io.on("connection", socket => {
    socket.on("join_channel", data => {
       socket.join(data.room);
+      saveDB("./bdRooms.json", data);
       socket.emit("join_channel", data);
    });
 
    socket.on("new message", data => {
+      // console.log(data);
       socket.to(data.room).emit("msg recibido", data);
    });
 });
