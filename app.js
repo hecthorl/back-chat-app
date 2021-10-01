69e55381db5d62e73ae7a4f6e29bf6a7246f0c83
hecthorl <hlvm03@gmail.com>
2021-10-01 12:39:58 -0500

    tres tres tres

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+// Esto debe ser ASYNC o no funciona
 module.exports = async function (fastify, opts) {
    fastify.get("/", (req, res) => {
       res.code(200).send({ Gaaaaa: 1111111111 });
+   });
+   fastify.get("/roomId", (req, res) => {
+      res.code(200).send({ Gaaaaa: 1111111111 });
+   });
+   fastify.post("/", (req, res) => {
+      res.code(201).send({ Gaaaaa: 1111111111 });
    });
 };
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 // Require the framework
 const Fastify = require("fastify");
+const cors = require("fastify-cors");
+const socketsIO = require("fastify-socket.io");
+// const mongoDB = require('fastify-mongodb')
 
 // Instantiate Fastify with some config
 const app = Fastify({
@@ -9,10 +12,27 @@ const app = Fastify({
    pluginTimeout: 10000,
 });
 
+const config = {
+   origin: ["https://front-chat-app.vercel.app/", "http://localhost:3000"],
+   methods: ["GET", "PUT", "POST"],
+};
+
 // Register your application as a normal plugin.
+app.register(cors, config);
+app.register(socketsIO, {
+   cors: {
+      origin: ["https://front-chat-app.vercel.app/", "http://localhost:3000"],
+      methods: ["GET", "PUT", "POST"],
+   },
+});
 app.register(require("./app.js"));
 
-// Start listening.
+app.ready().then(() => {
+   app.io.on("connection", socket => {
+      console.log(socket.id);
+   });
+});
+
 app.listen(process.env.PORT || 4000, "0.0.0.0", err => {
    if (err) {
       app.log.error(err);
